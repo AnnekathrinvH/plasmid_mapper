a4e7e094b84dd4a8d1540a4a9f71e435c387741a
ste <stefan.kosc@gmail.com>
2016-10-11 15:26:20 +0200

    datamodule fits features to opposite strand

--- datamodule.js
+++ datamodule.js
@@ -1,6 +1,31 @@
 const features = require('./features.json');
 const seq = require('./pcDNA3_1.json');
 
+getOppositeStrand = function(sequence) {
+    var oppStrand = '';
+
+    for (var i = 0; i < seq.seq.length; i++) {
+        switch(seq.seq[i]) {
+            case 'C':
+            oppStrand += 'G';
+            break;
+            case 'T':
+            oppStrand += 'A';
+            break;
+            case 'A':
+            oppStrand += 'T';
+            break;
+            case 'G':
+            oppStrand += 'C';
+            default:
+            break;
+        }
+    }
+    return oppStrand;
+}
+var oppStrand = getOppositeStrand(seq);
+
+
 getIndices = function(sequence, feature) {
 
     sequence = sequence.concat(sequence.slice(0, feature.length));
@@ -19,12 +44,15 @@ getIndices = function(sequence, feature) {
     return indices;
 }
 
+
+var indexOfFeatureOppStrand = [];
 var indexOfFeature = [];
 var data = [];
 
 for (var i in features) {
 
     indexOfFeature.push(getIndices(seq.seq, features[i].seq));
+    indexOfFeatureOppStrand.push(getIndices(oppStrand, features[i].seq));
 
 }
 
@@ -33,7 +61,8 @@ for (var i in features) {
     var n = {
         featureId: features[i].id,
         featureLength: features[i].seq.length,
-        position: indexOfFeature[i-1]
+        position: indexOfFeature[i-1],
+        oppPosition: indexOfFeatureOppStrand[i-1]
 
     }
     data.push(n);
@@ -44,5 +73,8 @@ var dataToViz = {
     seqLength: seq.seq.length,
     features: data
 }
-
 console.log(dataToViz)
+console.log(indexOfFeature);
+console.log('');
+console.log('');
+console.log(indexOfFeatureOppStrand);
