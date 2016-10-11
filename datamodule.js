a7fa0f9c3906519c9ef8fa8301ef1e52c9b09685
ste <stefan.kosc@gmail.com>
2016-10-11 13:53:58 +0200

    datamodule return array with fitted data

--- datamodule.js
+++ datamodule.js
@@ -1,32 +1,48 @@
 const features = require('./features.json');
 const seq = require('./pcDNA3_1.json');
 
-console.log(seq.seq.length)
-
-
 getIndices = function(sequence, feature) {
 
+    sequence = sequence.concat(sequence.slice(0, feature.length));
     var indices = [];
     var index = sequence.indexOf(feature);
     var prev = 0;
+
     while (index > -1) {
        indices.push(index + prev);
        prev += index + feature.length;
        sequence = sequence.slice(index + feature.length);
        index = sequence.indexOf(feature);
+
     }
 
     return indices;
 }
 
 var indexOfFeature = [];
+var data = [];
 
 for (var i in features) {
 
     indexOfFeature.push(getIndices(seq.seq, features[i].seq));
 
 }
-//it should br stored as an array of objects where 'key' is id and 'value' array or object of indexes
-for (var i = 0; indexOfFeature[i]; i++) {
-    console.log(indexOfFeature[i])
+
+for (var i in features) {
+
+    var n = {
+        featureId: features[i].id,
+        featureLength: features[i].seq.length,
+        position: indexOfFeature[i-1]
+
+    }
+    data.push(n);
+
 }
+
+var dataToViz = {
+    seqLength: seq.seq.length,
+    features: data
+}
+
+console.log(dataToViz)
